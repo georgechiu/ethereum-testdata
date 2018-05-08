270bf85a2402dda5bfec441e18126575b0a03b26
BALUNSZ\george.chiu <george.chiu@baluntek.com>
2018-05-08 18:08:51 +0800

    Update test case.

--- test/TestTestdata.sol
+++ test/TestTestdata.sol
@@ -2,15 +2,20 @@ pragma solidity ^0.4.21;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
+import "../contracts/strings.sol";
 import "../contracts/TestdataFactory.sol";
 
 contract TestTestdata {
+    using strings for *;
     TestdataFactory testdata = TestdataFactory(DeployedAddresses.TestdataFactory());
 
     function testCreateTestdata() public {
         uint oldCount = testdata.getTestdataCount(msg.sender);
         testdata.createTestdata("Test file", 256);
         uint newCount = testdata.getTestdataCount(msg.sender);
-        Assert.equal(oldCount + 1, newCount, "Test data created successful!");
+        string msg = "Old count: ".toSlice().concat(new string(oldCount).toSlice());
+        msg = msg.toSlice().concat(", new count: ".toSlice());
+        msg = msg.toSlice().concat(new string(newCount).toSlice());
+        Assert.equal(oldCount + 1, newCount, msg);
     }
 }
